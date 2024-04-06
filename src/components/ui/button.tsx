843cea373df4a1026b151c9938c80b873e0899ff
wonkyum-kim <dodo12120@naver.com>
2024-04-06 20:33:45 +0900

    feat: Button variant에 next 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,10 +18,10 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        next: 'rounded-lg text-white',
+        next: 'rounded-lg text-white bg-[#333333] hover:bg-[#333333]/40',
       },
       size: {
-        default: 'bg-[#333333] h-[52px] w-full mb-4', // TODO: margin
+        default: 'h-[52px] w-full',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
