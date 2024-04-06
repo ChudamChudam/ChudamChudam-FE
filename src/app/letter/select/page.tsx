c5dd2fa1501860a296830bff92179db108b79c27
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-04-07 00:46:33 +0900

    [FEAT] 온보딩 페이지 UI 구현  (#7)
    * :sparkles: Input 컴포넌트 설치 및 수정
    
    * :sparkles: Label 컴포넌트 설치
    
    * :hammer_and_wrench: radix-ui/react-label 의존성 설치
    
    * :lipstick: 온보딩 인풋 UI 구현
    
    * :construction: 임시 버튼
    
    * :sparkles: 공통 타이포 컴포넌트 구현
    
    * :lipstick: 메인 페이지 와이어 프레임 구현
    
    * :sparkles: Description 컴포넌트 공통 컴포넌트 Typo로 구현
    
    * :lipstick: 온보딩 페이지 구현
    
    * :bug: 컨플릭트 해결

--- src/app/letter/select/page.tsx
+++ src/app/letter/select/page.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { Button } from '@/components/ui/button';
-import { Header } from '@/components/layout/header';
-import { Description } from '@/components/layout/description';
-import { LetterPreview } from '@/components/features/letter/select/letterPreview';
-import { LetterCardCarousel } from '@/components/features/letter/select/letterCardCarousel';
+import { LetterCardCarousel } from "@/components/features/letter/select/letterCardCarousel";
+import { LetterPreview } from "@/components/features/letter/select/letterPreview";
+import { Description } from "@/components/layout/description";
+import { Header } from "@/components/layout/header";
+import { Button } from "@/components/ui/button";
 
 const LetterSelectPage = () => {
   return (
-    <div className='flex flex-col items-center h-dvh relative'>
+    <div className="flex flex-col items-center h-dvh relative">
       <Header prev />
       <Description>편지지를 선택해주세요.</Description>
       <LetterCardCarousel />
       <LetterPreview />
-      <Link href='/letter/write' className='absolute bottom-5 w-full'>
-        <Button variant='next'>다음</Button>
+      <Link href="/letter/write" className="absolute bottom-5 w-full">
+        <Button variant="next">다음</Button>
       </Link>
     </div>
   );
