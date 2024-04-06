8b0b8616b08a13efdd25aae87cca18cb0ad94bb2
wonkyum-kim <dodo12120@naver.com>
2024-04-07 00:56:18 +0900

    refactor: string을 리터럴 타입으로 변경하기

--- src/components/features/letter/flip/letterFace.tsx
+++ src/components/features/letter/flip/letterFace.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 import styles from '@/components/features/letter/flip/letterFlip.module.css';
 
-export const LetterFace = ({ side }: { side: string }) => {
+export const LetterFace = ({ side }: { side: 'front' | 'back' }) => {
   // TODO: 편지를 작성한 데이터를 받아오면 다시 수정하기
   return (
     <div
