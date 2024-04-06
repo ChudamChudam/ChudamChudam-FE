e7c6bb3500332e542f2abc4d4dd79118da70e181
codyman0 <hys83751952@gmail.com>
2024-04-06 20:12:59 +0900

    :lipstick: 온보딩 인풋 UI 구현

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import OnboardingInputForm from "@/components/features/onboarding/OnboardingInputForm";
+
+const OnboardingPage = () => {
+  return (
+    <main>
+      <OnboardingInputForm />
+    </main>
+  );
+};
+
+export default OnboardingPage;
---
+++ src/components/features/onboarding/OnboardingInputForm.tsx
@@ -0,0 +1,19 @@
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/label";
+
+const OnboardingInputForm = () => {
+  return (
+    <section className="flex flex-col gap-8">
+      <div>
+        <Label className="text-gray-500">TO.</Label>
+        <Input placeholder="받는 이의 이름을 알려주세요." />
+      </div>
+      <div>
+        <Label className="text-gray-500">FROM.</Label>
+        <Input placeholder="당신의 이름을 알려주세요." />
+      </div>
+    </section>
+  );
+};
+
+export default OnboardingInputForm;
