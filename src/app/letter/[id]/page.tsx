9d855de01900cfc6421bff195117c4c17cb5ac60
wonkyum-kim <dodo12120@naver.com>
2024-04-07 00:38:15 +0900

    page: 편지 읽기 페이지

--- src/app/letter/[id]/page.tsx
+++ src/app/letter/[id]/page.tsx
@@ -1,8 +1,12 @@
-import ShareButtonList from "@/components/features/letter/ShareButtonList";
+import ShareButtonList from '@/components/features/letter/ShareButtonList';
+import { LetterFlip } from '@/components/features/letter/flip/letterFlip';
+import { Header } from '@/components/layout/header';
 
 const LetterDetailPage = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center h-dvh'>
+      <Header home />
+      <LetterFlip />
       <ShareButtonList />
     </div>
   );
