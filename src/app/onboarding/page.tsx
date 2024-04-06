231635ec326432c4385fab8a0771fe0ea7ae318f
codyman0 <hys83751952@gmail.com>
2024-04-06 20:15:18 +0900

    :construction: 임시 버튼

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,9 +1,11 @@
 import OnboardingInputForm from "@/components/features/onboarding/OnboardingInputForm";
+import Link from "next/link";
 
 const OnboardingPage = () => {
   return (
     <main>
       <OnboardingInputForm />
+      <Link href="/letter/select">다음</Link>
     </main>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main>123</main>;
-}
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <main>
+      HomePage
+      <Link href="/onboarding">다음으로</Link>
+    </main>
+  );
+};
+
+export default HomePage;
