7e001911a78181edd129bd189d9ff0d9b542121a
wonkyum-kim <dodo12120@naver.com>
2024-04-06 23:00:10 +0900

    feat: nextError variant추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,8 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        next: 'rounded-lg text-white bg-[#333333]',
+        next: 'rounded-lg text-white bg-gray-700',
+        nextError: 'rounded-lg text-white bg-gray-300',
       },
       size: {
         default: 'h-[52px] w-full',
