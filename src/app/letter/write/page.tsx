c8e60e571e2fa2e74708a7d52c615ea41a68d53c
DESKTOP-QR42JKC\wyswh <wyswhsl21@naver.com>
2024-04-06 23:28:08 +0900

    [FEAT]: Bottom Sheet 만들기 전 커밋

--- src/app/letter/write/page.tsx
+++ src/app/letter/write/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { spacing } from "./../../../styles/theme/spacing";
 import {
   Card,
   CardContent,
@@ -9,12 +8,23 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import tema from "@/assets/images/letter/tema1.png";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
-import Arrow_left from "@/assets/images/Arrow_left.svg";
-import Arrow_right from "@/assets/images/Arrow_right.svg";
-import uploadimg from "@/assets/images/upload.svg";
-import edit from "@/assets/images/letter/edit.svg";
+import Tema from "@/assets/images/letter/tema1.png";
+
+import Arrow_Left from "@/assets/images/Arrow_left.svg";
+import Arrow_Right from "@/assets/images/Arrow_right.svg";
+import UploadImg from "@/assets/images/upload.svg";
+import EditImg from "@/assets/images/letter/edit.svg";
 import Image from "next/image";
 
 import { AvatarImage, Avatar } from "@/components/ui/avatar";
@@ -24,25 +34,41 @@ const Page = () => {
     <Card className="max-h-screen min-h-screen">
       <CardHeader className=" flex flex-row w-full h-[60px] justify-between items-center">
         <div className="flex gap-1">
-          <Image className=" cursor-pointer" alt="img" src={Arrow_left} />
-          <Image className=" cursor-pointer" alt="img" src={Arrow_right} />
+          <Image className=" cursor-pointer" alt="img" src={Arrow_Left} />
+          <Image className=" cursor-pointer" alt="img" src={Arrow_Right} />
         </div>
         <div>
-          <Image className=" cursor-pointer" alt="img" src={uploadimg} />
+          <Image className=" cursor-pointer" alt="img" src={UploadImg} />
         </div>
       </CardHeader>
       <CardContent className="flex flex-col items-start w-full h-[448px] max-h-[448px] px-[18px] py-[20px] relative rounded-[8px] z-0">
-        <div className=" flex items-center justify-end w-full absolute z-10">
-          <div className=" rounded-[999px] w-[56px] h-[56px] flex items-center justify-center bg-[#BDBDBD]">
-            <Image src={edit} alt="img" />
+        <div className="w-full max-w-[375px] z-10">
+          <div className=" flex items-center justify-end w-full  z-10">
+            <div className=" rounded-[999px] w-[56px] h-[56px] flex items-center justify-center bg-[#BDBDBD]">
+              <Image
+                className=" cursor-pointer w-[21.45px] h-[21.45px]"
+                src={EditImg}
+                alt="img"
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          <span>To. E와I-DLE</span>
+          <div className="w-full flex flex-row items-center justify-center mt-[20px]">
+            <div className="flex flex-col bg-[#fff] opacity-40 rounded-[8px] w-[296px] h-[263px] px-[16px] py-[12px]">
+              <span className=" text-black">To. E와I-DLE</span>
+              <textarea
+                placeholder="추억을 담은 편지를 작성해보세요"
+                maxLength={300}
+                className=" resize-none border-none h-[170px] mt-[13px] outline-none"
+              />
+              <div className=" w-full flex justify-end">
+                <span className=" text-black">From.마리</span>
+              </div>
+            </div>
+          </div>
         </div>
         <Image
           alt="tema"
-          src={tema}
+          src={Tema}
           layout="fill"
           objectFit="cover"
           objectPosition="center"
