5b864b016287599b2a3509cb787f22d2b13c873e
wonkyum-kim <dodo12120@naver.com>
2024-04-06 20:35:48 +0900

    page: letter select page

--- src/app/letter/select/page.tsx
+++ src/app/letter/select/page.tsx
@@ -1,18 +1,19 @@
 import { Button } from '@/components/ui/button';
 import { Header } from '@/components/layout/header';
-import { BackButton } from '@/components/layout/backButton';
+import { Description } from '@/components/layout/description';
 import { LetterPreview } from '@/components/features/letter/select/letterPreview';
 import { LetterCardCarousel } from '@/components/features/letter/select/letterCardCarousel';
 
-export default function LetterSelectPage() {
+const LetterSelectPage = () => {
   return (
-    // TODO: padding
-    <div className='flex flex-col items-center w-full h-dvh p-4'>
-      <BackButton />
-      <Header>편지지를 선택해주세요.</Header>
+    <div>
+      <Header back />
+      <Description>편지지를 선택해주세요.</Description>
       <LetterCardCarousel />
       <LetterPreview />
       <Button variant='next'>다음</Button>
     </div>
   );
-}
+};
+
+export default LetterSelectPage;
