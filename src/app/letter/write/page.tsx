163c6d3536ed011f8a36a0aac83d12bf838f55da
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-04-07 20:40:50 +0900

    [REFACTOR] 편지 쓰기 페이지 UI 레이아웃 정렬  (#15)
    * :broom: package.json에 해당하는 의존성 설치
    
    * :recycle: 사용하지 않는 변수 및 불필요한 주석 삭제
    
    * :recycle: 헤더 컴포넌트 카드 밖으로 이동
    
    * :recycle: 동료가 만든 컴포넌트 활용하여 구현하도록 변경
    
    * :recycle: letter/write 페이지 레이아웃 설정
    
    * :bug: 카드 max-h-screen으로 인한 긴 카드 영역 제거를 위해 스타일링 변경
    
    * :lipstick: 디자인에 맞게 UI 수정

--- src/app/letter/write/page.tsx
+++ src/app/letter/write/page.tsx
@@ -1,41 +1,17 @@
 "use client";
+import { Card, CardContent } from "@/components/ui/card";
 import { useRef, useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
 
 import Tema from "@/assets/images/letter/tema1.png";
 
-import Arrow_Left from "@/assets/images/Arrow_left.svg";
-import Arrow_Right from "@/assets/images/Arrow_right.svg";
-import UploadImg from "@/assets/images/upload.svg";
-import EditImg from "@/assets/images/letter/edit.svg";
 import Image from "next/image";
 
-import { Button } from "@/components/ui/button";
 import { Header } from "@/components/layout/header";
-import {
-  FontItem,
-  FontItemType,
-  ItemType,
-  SelectItem,
-} from "@/constants/write";
+import { Button } from "@/components/ui/button";
+
+import { Description } from "@/components/layout/description";
+import { StepProgressBar } from "@/components/layout/stepProgressBar";
+import Link from "next/link";
 
 const Page = () => {
   const [select, setSelect] = useState("font"); //내가 선택하고 있는 아이템이 무엇인지.?
@@ -56,10 +32,6 @@ const Page = () => {
   const [letter, setLetter] = useState<[]>([]);
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  /**
-   *@function handleItemClick
-   *@description 아이템 클릭 핸들러
-   */
   const handleItemClick = (
     item: { name: string; value: string },
     type: string
@@ -76,142 +48,129 @@ const Page = () => {
         break;
     }
   };
-  /**
-   *@function setRes
-   *@description 최종 실행 함수
-   */
-  const setRes = async () => {};
+
+  const handleSubmit = async () => {};
 
   return (
-    <Card className="max-h-screen min-h-screen">
-      <Header prev>
-        {/* <div className="flex gap-1">
-          <Image
-            className=" cursor-pointer"
-            alt="arrow-left"
-            src={Arrow_Left}
-          />
-          <Image
-            className=" cursor-pointer"
-            alt="arrow-right"
-            src={Arrow_Right}
-          />
-        </div>
-        <div>
-          <Image className=" cursor-pointer" alt="upload" src={UploadImg} />
-        </div> */}
-      </Header>
-      <div className=" px-[20px]">
-        <span>추억을 담은 편지를 작성해보세요.</span>
-      </div>
-      <CardContent className="flex flex-col items-start w-full h-[448px] max-h-[448px] px-[18px] py-[20px] relative rounded-[8px] z-0 mt-[24px]">
-        <div className="w-full max-w-[375px] z-10">
-          <div className=" flex items-center justify-end w-full  z-10">
-            {/* <div className=" rounded-[999px] w-[56px] h-[56px] flex items-center justify-center bg-[#BDBDBD]">
+    <main className="relative w-full h-dvh">
+      <Header prev />
+      <StepProgressBar />
+      <Description>추억을 담은 편지를 작성해보세요.</Description>
+      <Card className="mt-6">
+        <CardContent className="flex flex-col items-start w-full h-[448px] max-h-[448px] px-[18px] py-[20px] relative rounded-[8px] z-0">
+          <div className="w-full max-w-[375px] z-10">
+            <div className=" flex items-center justify-end w-full  z-10">
+              {/* <div className=" rounded-[999px] w-[56px] h-[56px] flex items-center justify-center bg-[#BDBDBD]">
               <Image
                 className=" cursor-pointer w-[21.45px] h-[21.45px]"
                 src={EditImg}
                 alt="img"
               />
             </div> */}
-          </div>
+            </div>
 
-          <div className="w-full flex flex-row items-center justify-center mt-[20px]">
-            <div className="flex flex-col bg-[#fff] bg-opacity-75 rounded-[8px] w-[296px] h-[263px] px-[16px] py-[12px]">
-              <span className=" text-black">To. E와I-DLE</span>
-              <textarea
-                ref={inputRef}
-                placeholder="추억을 담은 편지를 작성해보세요"
-                maxLength={300}
-                className=" resize-none border-none h-[170px] mt-[13px] outline-none"
-              />
-              <div className=" w-full flex justify-end">
-                <span className=" text-black">From.마리</span>
+            <div className="w-full flex flex-row items-center justify-center mt-[20px]">
+              <div className="flex flex-col bg-[#fff] bg-opacity-75 rounded-[8px] w-[296px] h-[263px] px-[16px] py-[12px]">
+                <span className=" text-black">To. E와I-DLE</span>
+                <textarea
+                  ref={inputRef}
+                  placeholder="추억을 담은 편지를 작성해보세요"
+                  maxLength={300}
+                  className=" resize-none border-none h-[170px] mt-[13px] outline-none"
+                />
+                <div className=" w-full flex justify-end">
+                  <span className=" text-black">From.마리</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Image
-          alt="tema"
-          src={Tema}
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          className="w-full  px-[20px] py-[22px] flex-col justify-start"
-        />
-      </CardContent>
-      <CardFooter className="flex-col flex w-full gap-[8px]">
-        <Drawer className="bg-white">
-          <DrawerTrigger asChild>
-            <Button variant="yellow">폰트설정하기</Button>
-          </DrawerTrigger>
-          <DrawerContent className=" bg-pink-sub w-[375px] h-[290px] mx-auto">
-            <div className="mx-auto w-[375px]">
-              {/* 버튼 클릭시 Drawer Content */}
-              <DrawerHeader>
-                <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-gray-700" />
-                <div className="w-full flex mt-4">
-                  {SelectItem?.map((v: ItemType, idx: number) => (
-                    <div
-                      onClick={() => setSelect(v.value)}
-                      key={v.name + idx + 1}
-                      className=" w-[125px] h-[48px] flex items-center justify-center cursor-pointer px-5"
-                    >
-                      <span>{v.name}</span>
-                    </div>
-                  ))}
-                </div>
-              </DrawerHeader>
-              <div className=" w-full flex flex-wrap px-5 pt-4 gap-5">
-                {select === "font" &&
-                  FontItem.map((v: FontItemType, idx: number) => (
-                    <div
-                      onClick={() => handleItemClick(v, "font")}
-                      key={v.name + idx + 1}
-                      className={`flex items-center justify-center w-24 h-24 bg-gray-100 bg-opacity-50 cursor-pointer font-${v.value}`}
-                    >
-                      {v.name}
-                    </div>
-                  ))}
-                {select === "sticker" &&
-                  ["스티커1", "스티커2", "스티커3"].map(
-                    (v: string, idx: number) => (
-                      <div
-                        onClick={() => alert("준비중입니다.")}
-                        key={v + idx + 1}
-                        className=" flex items-center justify-center w-24 h-24 bg-gray-100 bg-opacity-50 cursor-pointer"
-                      >
-                        {v}
-                      </div>
-                    )
-                  )}
-                {select === "background" &&
-                  ["배경1", "배경2", "배경3"].map((v: string, idx: number) => (
+          <Image
+            alt="tema"
+            src={Tema}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            className="w-full  flex-col justify-start"
+          />
+        </CardContent>
+      </Card>
+
+      {/* 관심사 분리가 필요합니다. */}
+      {/* <Drawer>
+        <DrawerTrigger asChild>
+         
+        </DrawerTrigger>
+        <DrawerContent className=" bg-pink-sub w-[375px] h-[290px] mx-auto">
+          <div className="mx-auto w-[375px]">
+            <DrawerHeader>
+              <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-gray-700" />
+              <div className="w-full flex mt-4">
+                {SelectItem?.map((v: ItemType, idx: number) => (
+                  <div
+                    onClick={() => setSelect(v.value)}
+                    key={v.name + idx + 1}
+                    className=" w-[125px] h-[48px] flex items-center justify-center cursor-pointer px-5"
+                  >
+                    <span>{v.name}</span>
+                  </div>
+                ))}
+              </div>
+            </DrawerHeader>
+            <div className=" w-full flex flex-wrap px-5 pt-4 gap-5">
+              {select === "font" &&
+                FontItem.map((v: FontItemType, idx: number) => (
+                  <div
+                    onClick={() => handleItemClick(v, "font")}
+                    key={v.name + idx + 1}
+                    className={`flex items-center justify-center w-24 h-24 bg-gray-100 bg-opacity-50 cursor-pointer font-${v.value}`}
+                  >
+                    {v.name}
+                  </div>
+                ))}
+              {select === "sticker" &&
+                ["스티커1", "스티커2", "스티커3"].map(
+                  (v: string, idx: number) => (
                     <div
                       onClick={() => alert("준비중입니다.")}
-                      key={v + idx}
+                      key={v + idx + 1}
                       className=" flex items-center justify-center w-24 h-24 bg-gray-100 bg-opacity-50 cursor-pointer"
                     >
                       {v}
                     </div>
-                  ))}
-              </div>
-
-              <DrawerFooter>
-                <DrawerClose asChild>
-                  <Button className=" mt-[14px] px-5" variant={"yellow"}>
-                    적용하기
-                  </Button>
-                </DrawerClose>
-              </DrawerFooter>
+                  )
+                )}
+              {select === "background" &&
+                ["배경1", "배경2", "배경3"].map((v: string, idx: number) => (
+                  <div
+                    onClick={() => alert("준비중입니다.")}
+                    key={v + idx}
+                    className=" flex items-center justify-center w-24 h-24 bg-gray-100 bg-opacity-50 cursor-pointer"
+                  >
+                    {v}
+                  </div>
+                ))}
             </div>
-          </DrawerContent>
-        </Drawer>
-        <Button onClick={() => setRes()} variant="next">
-          완료
-        </Button>
-      </CardFooter>
-    </Card>
+
+            <DrawerFooter>
+              <DrawerClose asChild>
+                <Button className=" mt-[14px] px-5" variant={"yellow"}>
+                  적용하기
+                </Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer> */}
+
+      <div className="w-full absolute bottom-5 flex flex-col gap-2 ">
+        <Button variant="yellow">폰트설정하기</Button>
+        <Link href={`/letter/id`} className="w-full">
+          <Button onClick={() => handleSubmit()} variant="next">
+            완료
+          </Button>
+        </Link>
+      </div>
+    </main>
   );
 };
 export default Page;
