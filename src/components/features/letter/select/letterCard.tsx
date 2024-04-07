3152ea43ad1e360af7b5159678958afc15084e62
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2024-04-07 20:31:24 +0900

    style: 변경된 디자인에 맞도록 css를 수정하였습니다. (#12)

--- src/components/features/letter/select/letterCard.tsx
+++ src/components/features/letter/select/letterCard.tsx
@@ -2,8 +2,8 @@ import { Card, CardContent } from '@/components/ui/card';
 
 export const LetterCard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Card className='w-[100px] h-[128px] hover:bg-gray-200 cursor-pointer'>
-      <CardContent className='flex w-full h-full justify-center items-center'>
+    <Card className='box-border w-[128px] h-[128px] hover:bg-gray-200 cursor-pointer border-gray-300'>
+      <CardContent className='flex w-full h-full justify-center items-center p-0'>
         {children}
       </CardContent>
     </Card>
