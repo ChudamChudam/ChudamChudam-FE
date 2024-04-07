6524247a3fc523147f325ea99ebb110726e38d80
wonkyum-kim <dodo12120@naver.com>
2024-04-07 15:40:08 +0900

    style: stepProgressBar의 애니메이션을 tailwind에 적용시켰습니다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,10 +81,15 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'increase-width': {
+          from: { width: '8px' },
+          to: { width: '32px' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'increase-width': 'increase-width 0.2s ease-in-out',
       },
     },
   },
