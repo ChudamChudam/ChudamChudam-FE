609bf66b6299a663d8a33ac6e1b40ff8064e3ab7
codyman0 <hys83751952@gmail.com>
2024-04-08 01:26:02 +0900

    :sparkles: 공유 받은 URL로 들어오면 보이는 컴포넌트 제어

--- src/components/features/letter/ShareButtonList.tsx
+++ src/components/features/letter/ShareButtonList.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { shareLink } from '@/utils/share';
-import { Button } from '@/components/ui/button';
 import KakaoShareButton from '@/components/features/letter/KakaoShareButton';
+import { Button } from '@/components/ui/button';
+import { shareLink } from '@/utils/share';
 
 const ShareButtonList = () => {
   const handleCopyClipboard = () => {
-    shareLink({ url: location.href });
+    shareLink({ url: `${location.href}?share=true` });
   };
   return (
-    <div className='flex flex-col gap-2 pt-5 w-full'>
-      <Button onClick={handleCopyClipboard} variant='yellow'>
+    <div className="flex flex-col gap-2 pt-5 w-full">
+      <Button onClick={handleCopyClipboard} variant="yellow">
         링크 공유
       </Button>
       <KakaoShareButton />
