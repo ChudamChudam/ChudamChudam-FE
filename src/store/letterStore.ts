16f969c70046e79924bc20f4154ef33ddfbbc763
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-04-07 21:08:30 +0900

    [FEAT] 인풋 기능 구현 (#16)
    * :sparkles: 인풋 기능 구현
    
    * :sparkles: 버튼 clsx 제거하여 수정

--- src/store/letterStore.ts
+++ src/store/letterStore.ts
@@ -2,6 +2,10 @@ import { create } from 'zustand';
 
 type LetterState = {
   selected: string;
+  sender : string;
+  receiver : string;
+  setReceiver: (value: string) => void;
+  setSender : (value: string) => void;
   setSelected: (image: string) => void;
 };
 
@@ -9,4 +13,8 @@ type LetterState = {
 export const useLetterStore = create<LetterState>()((set) => ({
   selected: '0',
   setSelected: (image) => set(() => ({ selected: image })),
+  sender : '',
+  setSender: (value) => set(() => ({ sender: value })),
+  receiver : '',
+  setReceiver: (value) => set(() => ({ receiver: value })),
 }));
