4574e8dd9f8b34a7210a29a3503d2d54b2098af6
wonkyum-kim <dodo12120@naver.com>
2024-04-07 15:47:46 +0900

    feat: 현재 진행정도를 표시하는 stepProgressBar를 만들었습니다.

---
+++ src/components/layout/stepProgressBar.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+
+import { PATH } from '@/constants/paths';
+
+export const StepProgressBar = () => {
+  const pathname = usePathname();
+
+  // TODO: /letter/write 페이지에 적용하기
+
+  return (
+    <div className='w-full h-2 flex gap-2 mt-2 mb-7'>
+      {PATH.map((item) => {
+        return (
+          <div
+            className={clsx(
+              'bg-gray-400 rounded-full w-2 h-2',
+              pathname === item && 'animate-increase-width w-8 bg-[#FDD16F]'
+            )}
+          />
+        );
+      })}
+    </div>
+  );
+};
---
+++ src/constants/paths.ts
@@ -0,0 +1,1 @@
+export const PATH = ['/onboarding', '/letter/select', '/letter/write'];
