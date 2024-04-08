1e453952be40ce27185096a40f6dcb0f6ed6c345
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2024-04-08 17:29:22 +0900

    [FEAT] /letter/write 페이지 (#28)
    * feat: write 페이지 작성
    
    * chore: 이전 작업물
    
    * eslint 수정
    
    * fix: useEffect dependency 추가

--- src/app/letter/write/page.tsx
+++ src/app/letter/write/page.tsx
@@ -1,158 +1,17 @@
-'use client';
-import { Card, CardContent } from '@/components/ui/card';
-import { useRef, useState } from 'react';
-import {
-  usePathname,
-  useRouter,
-  useSearchParams,
-  useSelectedLayoutSegment,
-  useSelectedLayoutSegments,
-  redirect,
-  notFound,
-} from 'next/navigation';
-import Tema from '@/assets/images/letter/tema1.png';
-
-import Image from 'next/image';
-import * as htmlToImage from 'html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 import { Header } from '@/components/layout/header';
-import { Button } from '@/components/ui/button';
-
-import { Description } from '@/components/layout/description';
 import { StepProgressBar } from '@/components/layout/stepProgressBar';
-import Link from 'next/link';
+import { Description } from '@/components/layout/description';
+import { Write } from '@/components/features/letter/write/write';
 
-import BottomSheet from '@/components/features/letter/drawer/BottomSheet';
-import axios from 'axios';
-import { post } from '@/lib/axios';
-
-const Page = () => {
-  const router = useRouter();
-  const ref = useRef<HTMLDivElement>(null); // 이미지 찍을 div ref
-  const [select, setSelect] = useState('font'); //내가 선택하고 있는 아이템이 무엇인지.?
-  const [fontItem, setFontItem] = useState<{ name: string; value: string }>({
-    name: '',
-    value: '',
-  });
-  const [sticker, setSticker] = useState<{ name: string; value: string }>({
-    name: '',
-    value: '',
-  });
-  const [background, setBackground] = useState<{ name: string; value: string }>({
-    name: '',
-    value: '',
-  });
-  const [letter, setLetter] = useState<[]>([]);
-  const inputRef = useRef<HTMLTextAreaElement>(null);
-
-  const handleSelectItemClick = (selectItem: string) => {
-    setSelect(selectItem);
-  };
-  const handleItemClick = (item: { name: string; value: string }, type: string) => {
-    switch (type) {
-      case 'font':
-        setFontItem(item);
-        break;
-      case 'sticker':
-        alert('준비중입니다.');
-        break;
-      case 'background':
-        alert('준비중입니다.');
-        break;
-    }
-  };
-  function base64toFile(base_data: any, filename: string) {
-    let arr = base_data.split(','),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]),
-      n = bstr.length,
-      u8arr = new Uint8Array(n);
-
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return new File([u8arr], filename, { type: mime });
-  }
-  const handleSubmit = async () => {
-    if (ref.current === null) {
-      return;
-    }
-    const dataUrl = await toPng(ref.current, { includeQueryParams: true });
-    const file = base64toFile(dataUrl, 'chudom.img.png');
-    let formData = new FormData();
-    formData.append('file', file);
-    const submitRes = await post<FormData>(
-      '/letter',
-      {
-        formData,
-      },
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      },
-    );
-    if (submitRes) {
-      router.push('/letter/id');
-    }
-  };
-
+const WritePage = () => {
   return (
-    <main className="relative w-full h-dvh">
+    <div className="relative w-full h-dvh">
       <Header prev />
       <StepProgressBar />
-      <Description>추억을 담은 편지를 작성해보세요.</Description>
-      <Card className="mt-6">
-        <CardContent
-          ref={ref}
-          className="flex flex-col items-start w-full h-[448px] max-h-[448px] px-[18px] py-[20px] relative rounded-[8px] z-0"
-        >
-          <div className="w-full max-w-[375px] z-10">
-            <div className=" flex items-center justify-end w-full  z-10"></div>
-
-            <div className="w-full flex flex-row items-center justify-center mt-[20px]">
-              <div className="flex flex-col bg-[#fff] bg-opacity-75 rounded-[8px] w-[296px] h-[263px] px-[16px] py-[12px]">
-                <span className=" text-black">To. E와I-DLE</span>
-                <textarea
-                  ref={inputRef}
-                  placeholder="추억을 담은 편지를 작성해보세요"
-                  maxLength={300}
-                  className=" resize-none border-none h-[170px] mt-[13px] outline-none bg-transparent"
-                />
-                <div className=" w-full flex justify-end">
-                  <span className=" text-black">From.마리</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <Image
-            alt="tema"
-            src={Tema}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-            className="w-full  flex-col justify-start"
-          />
-        </CardContent>
-      </Card>
-
-      {/* 관심사 분리가 필요합니다. */}
-
-      <div className="w-full absolute bottom-5 flex flex-col gap-2 ">
-        <BottomSheet
-          select={select}
-          handleSelectItem={handleSelectItemClick}
-          handleItemClick={handleItemClick}
-        />
-
-        {/* <Link href={`/letter/id`} className="w-full"> */}
-        <Button onClick={() => handleSubmit()} variant="next">
-          완료
-        </Button>
-        {/* </Link> */}
-      </div>
-    </main>
+      <Description>추억을 담은 편지를 작성해보세요</Description>
+      <Write />
+    </div>
   );
 };
-export default Page;
+
+export default WritePage;
---
+++ src/components/features/letter/write/write.tsx
@@ -0,0 +1,128 @@
+'use client';
+
+import Image from 'next/image';
+import { ChangeEventHandler, useRef, useState } from 'react';
+import { toPng } from 'html-to-image';
+import { useRouter } from 'next/navigation';
+
+import { useLetterStore } from '@/store/letterStore';
+import BottomSheet from '../drawer/BottomSheet';
+import { Button } from '@/components/ui/button';
+import { post } from '@/lib/axios';
+
+export const Write = () => {
+  const router = useRouter();
+  const selected = useLetterStore((state) => state.selected);
+  const sender = useLetterStore((state) => state.sender);
+  const receiver = useLetterStore((state) => state.receiver);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const resultRef = useRef<HTMLDivElement>(null);
+  const [length, setLength] = useState(0);
+  const [select, setSelect] = useState('font'); //내가 선택하고 있는 아이템이 무엇인지.?
+  const [fontItem, setFontItem] = useState<{ name: string; value: string }>({
+    name: '',
+    value: '',
+  });
+
+  const handleSelectItemClick = (selectItem: string) => {
+    setSelect(selectItem);
+  };
+
+  const handleItemClick = (item: { name: string; value: string }, type: string) => {
+    switch (type) {
+      case 'font':
+        setFontItem(item);
+        break;
+      case 'sticker':
+        alert('준비중입니다.');
+        break;
+      case 'background':
+        alert('준비중입니다.');
+        break;
+    }
+  };
+
+  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    if (!inputRef.current) return;
+    setLength(event.target.value.length);
+  };
+
+  function base64toFile(base_data: any, filename: string) {
+    let arr = base_data.split(','),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+  }
+
+  const handleSubmit = async () => {
+    if (resultRef.current === null) {
+      return;
+    }
+    const dataUrl = await toPng(resultRef.current, { includeQueryParams: true });
+    console.log(dataUrl);
+    const file = base64toFile(dataUrl, 'chudom.img.png');
+    let formData = new FormData();
+    formData.append('file', file);
+    const submitRes = await post<FormData>(
+      '/letter',
+      {
+        formData,
+      },
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    console.log(submitRes);
+    if (submitRes) {
+      router.push(`/letter/${submitRes.data.get('letterId')}`);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="mt-6 w-full h-[448px] flex items-center justify-center relative"
+        ref={resultRef}
+      >
+        <Image
+          src={selected}
+          width={336}
+          height={448}
+          alt="letter"
+          className="object-cover w-[336px] h-[448px]"
+        />
+        <div className="absolute top-[132px] left-[50%] -translate-x-1/2 w-[296px] h-[262px]">
+          <div className="w-full h-[37px]py-2 px-4">To. {receiver}</div>
+          <textarea
+            className="w-full h-[166px] py-2 px-4 outline-none placeholder:text-gray-500 resize-none"
+            placeholder="추억을 담은 편지를 작성해보세요"
+            maxLength={150}
+            ref={inputRef}
+            onChange={handleChange}
+          />
+          <div className="w-full h-[22px] px-4 text-right">{length}/200</div>
+          <div className="w-full h-[37px] py-2 px-4 text-right">From. {sender}</div>
+        </div>
+      </div>
+      <div className="absolute bottom-5 w-full flex flex-col gap-2">
+        <BottomSheet
+          select={select}
+          handleSelectItem={handleSelectItemClick}
+          handleItemClick={handleItemClick}
+        />
+        <Button variant="white" onClick={handleSubmit}>
+          작성 끝내기
+        </Button>
+      </div>
+    </>
+  );
+};
