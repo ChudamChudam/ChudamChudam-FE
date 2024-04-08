a79a234770045365c286d838049dd33ac2d0ad9f
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-04-08 20:47:11 +0900

    :bug: 코드 정리 (#36)

--- src/components/features/letter/flip/letterFlip.tsx
+++ src/components/features/letter/flip/letterFlip.tsx
@@ -5,11 +5,8 @@ import { useState } from 'react';
 import { Letter } from '@/components/features/letter/flip/letter';
 import { LetterFace } from '@/components/features/letter/flip/letterFace';
 import { Scene } from '@/components/features/letter/flip/scene';
-import { useGetLetterResult } from '@/hooks/useGetLetterResult';
 
 export const LetterFlip = () => {
-  // const { data } = useGetLetterResult(1);
-
   const [isFront, setIsFront] = useState(true);
 
   const handleFlip = () => {
