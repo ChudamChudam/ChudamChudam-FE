aa4b2252b40154aa670dae253308ae4174ef0273
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-04-08 16:16:10 +0900

    :white_check_mark: 재 배포를 위한 임시 커밋 (#26)

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -4,6 +4,7 @@ import { LetterResultType } from "@/types/letter";
 /**
  * @description 편지지 가지고 오는 API 
  */
+
 export const getLetterResult = async (id:number) => {
     const response = await get<LetterResultType>(`/letter/${id}`);
     return response.data;
