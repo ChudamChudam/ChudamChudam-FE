d0ecbed79c2e7351ab8808de56609e472046952e
wonkyum-kim <dodo12120@naver.com>
2024-04-08 14:31:24 +0900

    style: Label text 색 변경

--- src/components/features/onboarding/OnboardingInputForm.tsx
+++ src/components/features/onboarding/OnboardingInputForm.tsx
@@ -17,7 +17,7 @@ const OnboardingInputForm = () => {
   return (
     <section className="flex flex-col gap-8 pt-7">
       <div>
-        <Label className="text-gray-500">TO.</Label>
+        <Label className="text-gray-700">TO.</Label>
         <Input
           placeholder="받는 이의 이름을 알려주세요."
           value={receiver}
@@ -26,7 +26,7 @@ const OnboardingInputForm = () => {
         />
       </div>
       <div>
-        <Label className="text-gray-500">FROM.</Label>
+        <Label className="text-gray-700">FROM.</Label>
         <Input
           value={sender}
           placeholder="당신의 이름을 알려주세요."
