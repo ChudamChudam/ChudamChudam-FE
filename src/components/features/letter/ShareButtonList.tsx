0eee7c6b65fcc65219f867f338a0811eedf15046
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-04-08 20:08:34 +0900

    [REFACTOR] 마지막 간단한 리팩터링 진행  (#35)
    * :bug: 반대 조건 설정을 통해 공유를 통해 들어온 유저에게 다른 컴포넌트 렌더링
    
    * :lipstick: :bug: 메인 페이지 디자인 수정
    
    * :sparkles: formdata로 보내지 않고 일반 데이터 형식으로 변경

--- src/components/features/letter/ShareButtonList.tsx
+++ src/components/features/letter/ShareButtonList.tsx
@@ -12,6 +12,7 @@ const ShareButtonList = () => {
     shareLink({ url: `${location.href}?share=true` });
   };
 
+  console.log('isEnterBySharedUrl', isEnterBySharedUrl);
   const handleDownload = () => {
     // TODO: 다운로드 기능 구현하기
   };
@@ -21,7 +22,7 @@ const ShareButtonList = () => {
       <Button variant="yellow" onClick={handleDownload} className="cursor-not-allowed">
         저장하기
       </Button>
-      {isEnterBySharedUrl && (
+      {!isEnterBySharedUrl && (
         <div className="flex gap-2 ">
           <Button onClick={handleCopyClipboard} variant="green">
             링크 공유
